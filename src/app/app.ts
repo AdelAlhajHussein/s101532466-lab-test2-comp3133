ce0a4d667565533e281a6608f027067fb6a06024
AdelAlhajHussein <Adel.AlhajHussein@georgebrown.ca>
2026-04-08 12:14:27 -0400

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('s101532466-lab-test2-comp3133');
+}
